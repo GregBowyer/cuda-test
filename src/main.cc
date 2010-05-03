af6b6dafdc1f12fe4a570fdf4e825b3a3ccc103d
Greg Bowyer <gbowyer@shopzilla.com>
2010-05-03 11:44:20 +0100

    Replaced memset with calloc for linux builds
    Replaced the memset call that is being used to zero out memory obtained from
    malloc, with a calloc call that performs the same operation; but is slightly
    more portable across build environments.

--- src/main.cc
+++ src/main.cc
@@ -18,8 +18,7 @@ int main(int argc, char **argv) {
 	int wT = km.num_tokens();
 
 	unsigned int mem_size_result = sizeof(float) * wT * wT;
-	float* result = (float*) malloc(mem_size_result);
-	memset(result, 0, mem_size_result);
+	float* result = (float*) calloc(1, mem_size_result);
 
 	covariance(result, km.get_tokens(), km.get_intersections(), km.num_keywords());
 
